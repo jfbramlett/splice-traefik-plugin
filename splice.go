b609ac0aa121c87815baab3be152714868bba302
John Bramlett <john.bramlett@splice.com>
2022-02-17 17:46:46 -0500

    Removed extra plugins leaving just logging

--- splice.go
+++ splice.go
@@ -1,17 +1,11 @@
 package splicetraefikplugin
 
 import (
-	"bytes"
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"text/template"
-
-	"github.com/google/uuid"
 )
-
-const HeaderKey = "X-Request-Id"
 
 // Config the plugin configuration.
 type Config struct {
@@ -45,25 +39,8 @@ func New(_ context.Context, next http.Handler, config *Config, name string) (htt
 
 func (a *Splice) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	next := a.next
-	for key, value := range a.headers {
-		tmpl, err := a.template.Parse(value)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
 
-		writer := &bytes.Buffer{}
-
-		err = tmpl.Execute(writer, req)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		req.Header.Set(key, writer.String())
-	}
-
-	next = LoggingMiddleware(RequestIDMiddleware(SessionMiddleware(next)))
+	next = LoggingMiddleware(next)
 
 	next.ServeHTTP(rw, req)
 }
@@ -73,32 +50,5 @@ func LoggingMiddleware(h http.Handler) http.HandlerFunc {
 		log.Default().Printf("handling request: %s %s", r.Method, r.RequestURI)
 		h.ServeHTTP(w, r)
 		log.Default().Printf("completed request: %s %s", r.Method, r.RequestURI)
-	}
-}
-
-func RequestIDMiddleware(h http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Only generate a new request ID if it's not present in the Header
-		if reqUUID := r.Header.Get(HeaderKey); reqUUID == "" {
-			reqUUID = uuid.NewString()
-			log.Default().Printf("assigning generated uuid to request: %s", reqUUID)
-			r.Header.Set(HeaderKey, reqUUID)
-		}
-
-		h.ServeHTTP(w, r)
-	}
-}
-
-func SessionMiddleware(h http.Handler) http.HandlerFunc {
-	sessionMgr := NewSessionManager()
-	return func(w http.ResponseWriter, r *http.Request) {
-		user, err := sessionMgr.UserFromRequest(r.Context(), r)
-		if err != nil {
-			log.Default().Printf("failed trying to get user from cookie: %s", err.Error())
-		}
-		log.Default().Printf("assigning user uuid and id: %s %d", user.UUID, user.ID)
-		r.Header.Set("x-user-uuid", user.UUID)
-		r.Header.Set("x-user-id", fmt.Sprint(user.ID))
-		h.ServeHTTP(w, r)
 	}
 }
